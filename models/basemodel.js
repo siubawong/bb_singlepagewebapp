90fb99d7a9b570e4f6480325f770ac0ef025242d
siubawong <jcheung@jcheungs-MacBook-Pro.local>
2015-08-16 02:01:41 -0700

    added index.html and basemodel.js content

--- models/basemodel.js
+++ models/basemodel.js
@@ -1,7 +1,9 @@
 /**
    Object that describes the basic functionlaities of a model 
-
-
-
-
 **/
+
+var BaseModel = function(options)
+{
+  this.model = options.model;
+  this.parent = options.parent; 
+};
